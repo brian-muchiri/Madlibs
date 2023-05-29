44ebc511bc57848ba0112febacf8278577ed55ed
Brian Muchiri <115185660+Brian-Muchiri@users.noreply.github.com>
2023-05-29 13:56:04 +0300

    Add files via upload

---
+++ Madlibs/madlibs.py
@@ -0,0 +1,16 @@
+# #strings concatenation(ie how to put strings together)
+# #suppose that we were to create a string that says "subscribe to______"
+# youtuber = " spayo"
+# #a few ways to do this
+# print ("subscribe to" + youtuber)
+# print ("subscribe to {}".format(youtuber))
+# print(f"subscribe to{youtuber}")
+noun = input ("noun: ")
+verb1 =input("verb: ")
+verb2 =input("verb: ")
+verb3 = input ("verb3: ")
+famous_person = input("famous_person: ")
+madlib = f"My name is {noun}. I love {verb1} at my leisure time. I am also a {verb2}and {verb3} \
+I also idol {famous_person}."
+
+print(madlib)
